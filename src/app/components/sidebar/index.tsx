4bcf37c909fd3ccb71389b3c7ce414243bb7ff3f
migueelzz <miguel.hlemes.sgmail.com>
2024-03-14 00:08:27 -0300

    fix: scroll sidebar in the mobile version breaking responsive

--- src/app/components/sidebar/index.tsx
+++ src/app/components/sidebar/index.tsx
@@ -23,7 +23,7 @@ import { Button } from '../button'
 
 export default function Sidebar() {
   return (
-    <Collapisible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 overflow-y-auto border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 data-[state=open]:h-screen lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0 lg:data-[state=closed]:h-screen dark:border-zinc-800 dark:bg-zinc-900">
+    <Collapisible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 overflow-y-auto border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 data-[state=open]:h-screen dark:border-zinc-800 dark:bg-zinc-900 lg:right-auto lg:w-80 lg:overflow-hidden lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0 lg:data-[state=closed]:h-screen">
       <div className="flex items-center justify-between">
         <Logo />
         <Collapisible.Trigger asChild className="lg:hidden">
@@ -35,7 +35,7 @@ export default function Sidebar() {
 
       <Collapisible.Content
         forceMount
-        className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex"
+        className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex "
       >
         <Input.Root>
           <Input.Prefix>
